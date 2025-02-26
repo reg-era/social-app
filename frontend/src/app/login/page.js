6493dbbe8942665b4ce7066e9855d667c001855d
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-02-26 17:49:15 +0000

    cleaning fron end login singup

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -6,23 +6,13 @@ export default function Login() {
       <h1>Login</h1>
       <form className="login-form" action="/Home" method="POST">
         <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          required
-        />
+        <input type="email" name="email" id="email" required />
+
         <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          required
-        />
+        <input type="password" name="password" id="password" required />
+
         <button type="submit">Login</button>
-        <p>
-          Don't have an account? <a href="/signup">Signup</a>
-        </p>
+        <p>  Don't have an account? <a href="/signup">Signup</a></p>
       </form>
     </div>
   );
