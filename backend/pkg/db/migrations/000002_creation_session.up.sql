551b0de508af26fcebbf7caa6c47c0dade9be7ac
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-02-26 17:42:47 +0000

    add structure for session feature

---
+++ backend/pkg/db/migrations/000002_creation_session.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE sessions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    session_id TEXT NOT NULL UNIQUE,
+    user_id INTEGER NOT NULL,
+    expires_at DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
