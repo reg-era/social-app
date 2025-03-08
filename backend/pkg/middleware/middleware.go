d5a3fd01378979a1ca95e92c9478696a8a0f48a5
hadogen <hadogen01@proton.me>
2025-03-08 07:49:09 +0000

    cors put

--- backend/pkg/middleware/middleware.go
+++ backend/pkg/middleware/middleware.go
@@ -18,7 +18,7 @@ type API struct {
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
