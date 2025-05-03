c76c5bd0775da339a9b07c6a0e529fc894d58c38
yahya houssaini <houssyahya@gmail.com>
2025-05-03 14:43:25 +0100

    comment groups

--- backend/core/group_comment.go
+++ backend/core/group_comment.go
@@ -3,102 +3,149 @@ package core
 import (
 	"fmt"
 	"net/http"
+	"path"
 	"strconv"
 
 	"social/pkg/utils"
 )
 
-type group_Comment struct {
-	ID        int    `json:"CommentId"`
-	User      User   `json:"user"`
-	Content   string `json:"commentText"`
+type GroupComment struct {
+	ID        int    `json:"comment_id"`
+	GroupID   int    `json:"group_id"`
 	PostID    int    `json:"post_id"`
-	CreatedAt string `json:"commentTime"`
+	Username  string `json:"author_name"`
+	Content   string `json:"content"`
+	ImageUrl  string `json:"image_url"`
+	CreatedAt string `json:"comment_time"`
 }
 
 func (a *API) HandleGroupPostComments(w http.ResponseWriter, r *http.Request) {
-	userId := r.Context().Value("userID").(int)
-
+	userId, ok := r.Context().Value("userID").(int)
+	if !ok {
+		utils.RespondWithJSON(w, http.StatusUnauthorized, map[string]string{"error": "Unauthorized"})
+		return
+	}
 
 	switch r.Method {
 	case http.MethodPost:
-		postId, err := strconv.Atoi(r.FormValue("post_id"))
+		groupId, err := strconv.Atoi(r.FormValue("groupID"))
 		if err != nil {
-			utils.RespondWithJSON(w, http.StatusBadRequest, map[string]string{"error": "Invalid post ID"})
+			utils.RespondWithJSON(w, http.StatusBadRequest, map[string]string{"failed": "Invalid group_id"})
 			return
 		}
-		group_Id, err := strconv.Atoi(r.FormValue("group_id"))
+
+		postId, err := strconv.Atoi(r.FormValue("postID"))
 		if err != nil {
-			utils.RespondWithJSON(w, http.StatusBadRequest, map[string]string{"error": "Invalid post ID"})
+			utils.RespondWithJSON(w, http.StatusBadRequest, map[string]string{"failed": "Invalid post_id"})
 			return
 		}
 
 		content := r.FormValue("comment")
+		file, handler, err := r.FormFile("image")
+		var imagePath string
+		if err == nil {
+			imagePath, err = utils.UploadFileData(file, handler)
+			if err != nil {
+				utils.RespondWithJSON(w, http.StatusBadRequest, map[string]string{
+					"error": err.Error(),
+				})
+				return
+			}
+		}
 
-		commentId, err := a.Create(`INSERT INTO group_comments (post_id, user_id, content, group_id) VALUES (?, ?, ?,?)`, postId, userId, content, group_Id)
+		if imagePath != "" {
+			imagePath = path.Join("api/global/", imagePath)
+		}
+
+		commentId, err := a.Create(
+			`INSERT INTO group_comments (group_id, post_id, user_id, content, image_url) VALUES (?, ?, ?, ?, ?)`,
+			groupId,
+			postId,
+			userId,
+			content,
+			imagePath,
+		)
 		if err != nil {
-			fmt.Println(err)
-			utils.RespondWithJSON(w, http.StatusInternalServerError, map[string]string{"faild": "Status Internal Server Error"})
+			utils.RespondWithJSON(
+				w,
+				http.StatusInternalServerError,
+				map[string]string{"failed": "Status Internal Server Error"},
+			)
 			return
 		}
 
 		resComment := a.Read(`
-		SELECT users.firstname, users.lastname, users.email, users.avatarUrl, group_comments.id, group_comments.content, group_comments.created_at 
-		FROM group_comments
-		JOIN users ON group_comments.user_id = users.id
-		WHERE group_comments.id = ?
-		`, commentId)
+        SELECT u.firstname, u.lastname, c.id, c.content, c.image_url, c.created_at FROM group_comments c
+        JOIN users u ON user_id = u.id
+        WHERE c.id = ?`, commentId)
 
-		var comment group_Comment
-		if err := resComment.Scan(&comment.User.FirstName, &comment.User.LastName, &comment.User.Email, &comment.User.AvatarUrl, &comment.ID, &comment.Content, &comment.CreatedAt); err != nil {
-			fmt.Println(err)
-			utils.RespondWithJSON(w, http.StatusInternalServerError, map[string]string{"faild": "Status Internal Server Error"})
+		var comment GroupComment
+		var first, last string
+		if err := resComment.Scan(&first, &last, &comment.ID, &comment.Content, &comment.ImageUrl, &comment.CreatedAt); err != nil {
+			utils.RespondWithJSON(
+				w,
+				http.StatusInternalServerError,
+				map[string]string{"failed": "Status Internal Server Error"},
+			)
 			return
 		}
-
+		comment.Username = first + " " + last
 		utils.RespondWithJSON(w, http.StatusCreated, comment)
 
 	case http.MethodGet:
-
-		postId, err := strconv.Atoi(r.URL.Query().Get("post_id"))
-		if err != nil {
-			fmt.Println(err)
-			utils.RespondWithJSON(w, http.StatusBadRequest, map[string]string{"error": "Invalid post ID"})
+		groupId, err := strconv.Atoi(r.URL.Query().Get("groupID"))
+		if groupId == 0 || err != nil {
+			utils.RespondWithJSON(w, http.StatusBadRequest, map[string]string{"error": "Invalid group_id"})
 			return
 		}
 
-		data, err := a.ReadAll(`
-		SELECT users.firstname, users.lastname, users.email, users.avatarUrl, pc.id, pc.content, pc.created_at
-		FROM group_comments pc
-		JOIN users ON pc.user_id = users.id
-		WHERE pc.post_id = ?
-		ORDER BY pc.created_at DESC;
-		`, postId)
-		if err != nil {
-			fmt.Println(err)
-
-			utils.RespondWithJSON(w, http.StatusInternalServerError, map[string]string{"error": "Status Internal Server Error"})
+		postId, err := strconv.Atoi(r.URL.Query().Get("postID"))
+		if postId == 0 || err != nil {
+			utils.RespondWithJSON(w, http.StatusBadRequest, map[string]string{"error": "Invalid post_id"})
 			return
 		}
-		defer data.Close()
 
-		allComments := []group_Comment{}
-		for data.Next() {
-			var comment group_Comment
-			if err := data.Scan(&comment.User.FirstName, &comment.User.LastName, &comment.User.Email, &comment.User.AvatarUrl, &comment.ID, &comment.Content, &comment.CreatedAt); err != nil {
-			fmt.Println(err)
-				
-				utils.RespondWithJSON(w, http.StatusInternalServerError, map[string]string{"error": "Status Internal Server Error"})
+		page := 0
+		if param := r.URL.Query().Get("page"); param != "" {
+			page, err = strconv.Atoi(param)
+			if err != nil {
+				utils.RespondWithJSON(w, http.StatusBadRequest, map[string]string{"error": "Invalid page number"})
 				return
 			}
-			allComments = append(allComments, comment)
 		}
 
-		if err := data.Err(); err != nil {
+		dataRows, err := a.ReadAll(`
+        SELECT u.firstname, u.lastname, c.id, c.group_id, c.post_id, c.content, c.image_url, c.created_at FROM group_comments c
+        JOIN users u ON user_id = u.id
+        WHERE c.group_id = ? AND c.post_id = ?
+        ORDER BY c.created_at DESC
+        LIMIT 3 OFFSET (3 * ?)`, groupId, postId, page)
+		if err != nil {
 			fmt.Println(err)
-
-			utils.RespondWithJSON(w, http.StatusInternalServerError, map[string]string{"error": "Status Internal Server Error"})
+			utils.RespondWithJSON(
+				w,
+				http.StatusInternalServerError,
+				map[string]string{"error": "Status Internal Server Error"},
+			)
 			return
+		}
+		defer dataRows.Close()
+
+		allComments := []GroupComment{}
+		for dataRows.Next() {
+			var comment GroupComment
+			var first, last string
+			if err := dataRows.Scan(&first, &last, &comment.ID, &comment.GroupID, &comment.PostID, &comment.Content, &comment.ImageUrl, &comment.CreatedAt); err != nil {
+				fmt.Println(err)
+				utils.RespondWithJSON(
+					w,
+					http.StatusInternalServerError,
+					map[string]string{"failed": "Status Internal Server Error"},
+				)
+				return
+			}
+			comment.Username = first + " " + last
+			allComments = append(allComments, comment)
 		}
 
 		utils.RespondWithJSON(w, http.StatusOK, allComments)
--- backend/go.mod
+++ backend/go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.18.2
 	github.com/google/uuid v1.6.0
 	github.com/gorilla/websocket v1.5.3
-	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/mattn/go-sqlite3 v1.14.28
 	golang.org/x/crypto v0.35.0
 )
 
